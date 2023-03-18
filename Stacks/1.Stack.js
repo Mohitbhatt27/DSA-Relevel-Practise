5106c93f6fa74f5c9489ac9c8e0fcd0eac358745
Mohit Bhatt <mohit.peak@gmail.com>
2023-03-18 22:59:42 +0530

    Added implementation of Stacks using Classes

--- Stacks/1.Stack.js
+++ Stacks/1.Stack.js
@@ -1,59 +1,24 @@
 class Stack {
-  #data; // this data property will be accessible only inside the stack class not outside, this array stores the actual data
-  #top; // this parameter is also private and the value denotes the index of the top most element in the array
+  #arr;
   constructor() {
-    this.#data = [];
-    this.#top = -1;
+    this.#arr = [];
   }
-
-  isEmpty() {
-    return this.#top == -1;
+  push(x) {
+    this.#arr.push(x);
   }
-
-  peek() {
-    /**
-     * Time: O(1)
-     */
-    // this function returns the topmost element
-    if (this.isEmpty()) {
-      console.log('Stack is empty, cannot retrive an element');
-      return;
-    }
-    return this.#data[this.#top];
-  }
-
-  push(element) {
-    /**
-     * Time: O(1)
-     */
-    // this function actually helps to insert a new element in the stack
-    this.#top += 1;
-    this.#data[this.#top] = element;
-  }
-
   pop() {
-    /**
-     * Time: O(1)
-     */
-    // this function removes an element from the Stack
-    // we are not going to literally remove an element but just reduce the access to it
-    if (this.isEmpty()) {
-      console.log('Stack is empty, cannot remove an element');
-      return;
-    }
-    this.#data[this.#top] = undefined;
-    this.#top -= 1;
+    this.#arr.pop();
   }
-
-  display() {
-    console.log(this.#data);
+  top() {
+    if ((this.#arr.length == 0)) return undefined;
+    return this.#arr[this.#arr.length - 1];
   }
 }
-
 const st = new Stack();
-st.push(1);
-st.push(2);
-st.push(3);
+st.push(10);
+st.push(20);
+console.log(st.top());
+st.push(30);
+st.push(40);
 st.pop();
-st.push(4);
-st.display();
+console.log(st.top());

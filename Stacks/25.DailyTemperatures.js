d9d2b95da1a9fa4738dee32e3534efa8ced5eee1
Mohit Bhatt <mohit.peak@gmail.com>
2023-03-27 07:25:05 +0530

    Daily temperature code added, Montonic stack

---
+++ Stacks/25.DailyTemperatures.js
@@ -0,0 +1,18 @@
+var dailyTemperatures = function (temperatures) {
+  const n = temperatures.length;
+  const result = new Array(n).fill(0);
+  const stack = [];
+
+  for (let i = 0; i < n; i++) {
+    while (
+      stack.length &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      const index = stack.pop();
+      result[index] = i - index;
+    }
+    stack.push(i);
+  }
+
+  return result;
+};

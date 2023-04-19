a30e07aa297a6806ddd8e98e2ec6f8a05545628d
Mohit Bhatt <mohit.peak@gmail.com>
2023-04-19 21:23:57 +0530

    added insertion at any index

--- Stacks/5.InsertAtBottom_Stack.js
+++ Stacks/5.InsertAtBottom_Stack.js
@@ -12,4 +12,24 @@ function insertAtBottom(st, elem) {
   insertAtBottom(st, elem);
 
   st.push(temp);
+
+  return st;
 }
+
+// 7,8,9,0,3,4,5,6
+function insertAtIdx (st, elem, idx) {
+  if (st.length == idx) {
+    st.push(elem);
+    return;
+  }
+  let temp = st.pop();
+  insertAtIdx(st, elem, idx);
+  st.push(temp);
+  return st;
+}
+
+let st = [7,8,9,0,3,4,5,6];
+let elem = 2;
+let idx = 4;
+
+console.log(insertAtIdx(st, elem, idx));

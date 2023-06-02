6ab851991a28be81f12af159a341bf276491e875
Mohit Bhatt <mohit.peak@gmail.com>
2023-06-02 14:46:12 +0530

    minor fixes

--- Arrays/ArrayQuestions1[1].js
+++ Arrays/ArrayQuestions1[1].js
@@ -1,19 +1,20 @@
 //Given an array, write a program to reverse the same array, you should not create a new array
 
-function swap(arr,i,j){
-    let temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
-    return arr;
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+  return arr;
 }
-function reverseArray(arr){
-    let start=0, end=arr.length-1;
-    while(start<end){
-        swap(arr, start, end);
-        start++;
-        end--;
-    }
-    return arr;
+function reverseArray(arr) {
+  let start = 0,
+    end = arr.length - 1;
+  while (start < end) {
+    swap(arr, start, end);
+    start++;
+    end--;
+  }
+  return arr;
 }
 
-console.log(reverseArray([1,2,3,4,5,6]));
+console.log(reverseArray([1, 2, 3, 4, 5, 6]));
--- Arrays/Arrays.find()[1].js
+++ Arrays/Arrays.find()[1].js
@@ -5,7 +5,7 @@ If no values satisfy the testing function, undefined is returned.
 */
 
 const array1 = [5, 12, 8, 130, 44];
-const found = array1.find(element => element > 10);
+const found = array1.find((element) => element > 10);
 console.log(found);
 // expected output: 12
 
@@ -13,8 +13,8 @@ console.log(found);
 Let's say there is an object and you need to return the key for a particular value, this can be used.
 */
 
-let obj={one:1, two:2};
-const found1= Object.keys(obj).find(element => obj[element]==2);
+let obj = { one: 1, two: 2 };
+const found1 = Object.keys(obj).find((element) => obj[element] == 2);
 console.log(found1);
 
 //traversing in keys array, and finding the key wheb a particular element is found.

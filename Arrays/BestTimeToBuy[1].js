ccd1813d9e4a1711ca889944bb44d61bdfe4251a
Mohit Bhatt <mohit.peak@gmail.com>
2023-06-02 14:47:07 +0530

    stock buy

--- Arrays/BestTimeToBuy[1].js
+++ Arrays/BestTimeToBuy[1].js
@@ -1,20 +1,18 @@
 //LeetCode 121
 //121. Best Time to Buy and Sell Stock
 
-var maxProfit = function(prices) {
-    let profit_max = 0;
-    let curr_min = prices[0];
+var maxProfit = function (prices) {
+  let profit_max = 0;
+  let curr_min = prices[0];
 
-    for (let i = 1; i < prices.length; i++){
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] - curr_min > profit_max) {
+      profit_max = prices[i] - curr_min; //selling
+    }
 
-        if(prices[i] - curr_min > profit_max){
-                profit_max = prices[i] - curr_min;  //selling
-            }
-
-            if (prices[i] < curr_min){
-                curr_min = prices[i]; //and we buy on here
-            }
-            
-     }
-     return profit_max;;
- }
+    if (prices[i] < curr_min) {
+      curr_min = prices[i]; //and we buy on here
+    }
+  }
+  return profit_max;
+};

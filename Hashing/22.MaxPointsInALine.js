1db555813a8a0da473173c3aa4364456cc5f4477
Mohit Bhatt <mohit.peak@gmail.com>
2023-06-15 01:22:36 +0530

    points in a line max

--- Hashing/22.MaxPointsInALine.js
+++ Hashing/22.MaxPointsInALine.js
@@ -36,3 +36,6 @@ var maxPoints = function (points) {
 
   return res;
 };
+
+console.log(maxPoints([[1, 1], [2, 2], [3, 3], [4, 5]]));
+console.log(maxPoints([[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]]));
